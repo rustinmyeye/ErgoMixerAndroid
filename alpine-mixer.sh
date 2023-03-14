2f729e29c268eef498adef07586fe0594cc5782b
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-14 13:52:35 -0400

    Update alpine-mixer.sh

--- alpine-mixer.sh
+++ alpine-mixer.sh
@@ -13,33 +13,3 @@ cd profile.d
 curl https://raw.githubusercontent.com/rustinmyeye/ErgoMixerAndroid/main/mixer-startup.sh >> mixer.sh
 chmod +x mixer.sh
 clear
-
-##create firewall
-echo "Setting up the firewall"
-sleep 2
-
-# Flush existing rules
-iptables -F
-ip6tables -F
-
-# Allow loopback
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-
-# Allow incoming to port 9000 from loopback
-iptables -A INPUT -i lo -p tcp --dport 9000 -j ACCEPT
-
-# Block all other incoming traffic
-iptables -P INPUT DROP
-
-# Save rules to persist across reboots
-iptables-save > /etc/iptables/rules.v4
-
-# Repeat for IPv6
-ip6tables -A INPUT -i lo -j ACCEPT
-ip6tables -A OUTPUT -o lo -j ACCEPT
-ip6tables -A INPUT -i lo -p tcp --dport 9000 -j ACCEPT
-ip6tables -P INPUT DROP
-ip6tables-save > /etc/iptables/rules.v6
-
-clear
