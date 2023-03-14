4faaa54197d07d7eabb745e3c4a9d7901f13530f
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-13 23:09:07 -0400

    Update alpine-mixer.sh

--- alpine-mixer.sh
+++ alpine-mixer.sh
@@ -5,14 +5,15 @@ echo "Downloading stuff... please wait :)"
 apk add openjdk11 python3 curl --quiet
 clear
 
+#create startup script
+cd ..
+cd etc
+cd profile.d 
+curl https://raw.githubusercontent.com/rustinmyeye/ErgoMixerAndroid/main/mixer-startup.sh >> mixer.sh
+chmod +x mixer.sh
+clear
+
 ## Download .jar
 echo "- Retrieving latest Ergo Mixer release.."
 wget -O mixer.jar https://github.com/ergoMixer/ergoMixBack/releases/download/4.3.0/ergoMixer-4.3.0.jar
 clear
-
-## Start node
-echo "Starting the Mixer..."
-
-echo "Please visit https://0.0.0.0.9000/dashboard to start using Ergo Mixer!" 
-
-java -jar mixer.jar
