92a0768b95a35cddfc1ed262c3722f4311f3c01a
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-13 23:15:50 -0400

    Update alpine-mixer.sh

--- alpine-mixer.sh
+++ alpine-mixer.sh
@@ -12,8 +12,3 @@ cd profile.d
 curl https://raw.githubusercontent.com/rustinmyeye/ErgoMixerAndroid/main/mixer-startup.sh >> mixer.sh
 chmod +x mixer.sh
 clear
-
-## Download .jar
-echo "- Retrieving latest Ergo Mixer release.."
-wget -O mixer.jar https://github.com/ergoMixer/ergoMixBack/releases/download/4.3.0/ergoMixer-4.3.0.jar
-clear
