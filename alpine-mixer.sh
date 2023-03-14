32e564ebec9530aeaf79845368ee7999e147e35c
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-14 19:00:20 -0400

    Update alpine-mixer.sh

--- alpine-mixer.sh
+++ alpine-mixer.sh
@@ -3,7 +3,7 @@
 #download things
 echo "Downloading stuff... please wait :)"
 apk update --quiet
-apk add openjdk11 tmux python3 ufw wget --quiet
+apk add openjdk11 tmux iptables ip6tables ufw wget --quiet
 clear
 
 #create startup script
