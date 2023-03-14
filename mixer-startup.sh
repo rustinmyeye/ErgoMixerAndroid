bc302b89c7a0ac681ae1d7ab72db185cee9a9ab6
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-13 23:15:13 -0400

    Update mixer-startup.sh

--- mixer-startup.sh
+++ mixer-startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## Download .jar
+echo "- Retrieving latest Ergo Mixer release.."
+wget -O mixer.jar https://github.com/ergoMixer/ergoMixBack/releases/download/4.3.0/ergoMixer-4.3.0.jar
+clear
+
 ## Start node
 echo "Starting the Mixer..."
 
