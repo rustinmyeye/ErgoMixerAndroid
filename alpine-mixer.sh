b9863c0203d356ede5cc2df094ef78cf58b53a46
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-14 02:03:52 -0400

    Update alpine-mixer.sh

--- alpine-mixer.sh
+++ alpine-mixer.sh
@@ -12,3 +12,35 @@ cd profile.d
 curl https://raw.githubusercontent.com/rustinmyeye/ErgoMixerAndroid/main/mixer-startup.sh >> mixer.sh
 chmod +x mixer.sh
 clear
+
+##create firewall
+
+# Install iptables and ip6tables
+apk update --quiet
+apk add iptables ip6tables --quiet
+
+# Flush existing rules
+iptables -F
+ip6tables -F
+
+# Allow loopback traffic
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Block incoming traffic on port 9000
+iptables -A INPUT -p tcp --dport 9000 -j DROP
+
+# Block all other incoming traffic
+iptables -P INPUT DROP
+
+# Save rules to persist across reboots
+iptables-save > /etc/iptables/rules.v4
+
+# Repeat for IPv6
+ip6tables -A INPUT -i lo -j ACCEPT
+ip6tables -A OUTPUT -o lo -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 9000 -j DROP
+ip6tables -P INPUT DROP
+ip6tables-save > /etc/iptables/rules.v6
+
+clear
