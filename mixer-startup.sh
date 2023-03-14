eea06f1aea389dc02144f00f91baf6abd3e81cf5
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-14 13:53:03 -0400

    Update mixer-startup.sh

--- mixer-startup.sh
+++ mixer-startup.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+##create firewall
+echo "Setting up the firewall"
+sleep 2
+
+# Flush existing rules
+iptables -F
+ip6tables -F
+
+# Allow loopback
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Allow incoming to port 9000 from loopback
+iptables -A INPUT -i lo -p tcp --dport 9000 -j ACCEPT
+
+# Block all other incoming traffic
+iptables -P INPUT DROP
+
+# Save rules to persist across reboots
+iptables-save > /etc/iptables/rules.v4
+
+# Repeat for IPv6
+ip6tables -A INPUT -i lo -j ACCEPT
+ip6tables -A OUTPUT -o lo -j ACCEPT
+ip6tables -A INPUT -i lo -p tcp --dport 9000 -j ACCEPT
+ip6tables -P INPUT DROP
+ip6tables-save > /etc/iptables/rules.v6
+
+clear
+
 ## Download .jar
 echo "- Retrieving latest Ergo Mixer release.."
 wget -O mixer.jar https://github.com/ergoMixer/ergoMixBack/releases/download/4.3.0/ergoMixer-4.3.0.jar
