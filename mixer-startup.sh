0a7d56657e41693423ec0517843e577e000673eb
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-13 22:38:43 -0400

    Update mixer-startup.sh

--- mixer-startup.sh
+++ mixer-startup.sh
@@ -2,11 +2,7 @@
 
 ## Download .jar
 echo "- Retrieving latest Ergo Mixer release.."
-LATEST_MIXER_RELEASE=$(curl -s "https://api.github.com/repos/ergoMixer/ergoMixBack/releases/latest" | awk -F '"' '/tag_name/{print $4}')
-LATEST_MIXER_RELEASE_NUMBERS=$(echo ${LATEST_MIXER_RELEASE} | cut -c 2-)
-MIXER_DOWNLOAD_URL=https://github.com/ergoMixer/ergoMixBack/releases/download/${LATEST_ERGO_RELEASE}/ergoMixer-${LATEST_ERGO_RELEASE_NUMBERS}.jar
-echo "- Downloading Latest known Ergo Mixer release: ${LATEST_MIXER_RELEASE}."
-curl --silent -L ${MIXER_DOWNLOAD_URL} --output mixer.jar
+curl https://github.com/ergoMixer/ergoMixBack/releases/download/4.3.0/ergoMixer-4.3.0.jar >> mixer.jar
 
 ## Start node
 echo "Starting the Mixer..."
